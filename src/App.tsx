5c4dedb1abfae8bba707e57fecaa3a8afdc6effd
Robert Caw Brush <robertcawbrush@gmail.com>
2021-02-20 18:23:40 -0600

    init commit

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import './App.global.css';
+
+const Hello = () => {
+  return (
+    <div>
+     <h1>Get blasted eyyyyy</h1>
+    </div>
+  );
+};
+
+export default function App() {
+  return (
+    <Router>
+      <Switch>
+        <Route path="/" component={Hello} />
+      </Switch>
+    </Router>
+  );
+}
